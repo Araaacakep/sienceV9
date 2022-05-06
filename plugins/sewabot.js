4e41d8f41bfb673c4137d4b91e154222ebe79477
Araaacakep <100002720+Araaacakep@users.noreply.github.com>
2022-05-06 22:17:11 +0800

    Update sewabot.js

--- plugins/sewabot.js
+++ plugins/sewabot.js
@@ -11,8 +11,8 @@ let handler = async(m, { conn }) => {
 Silahkan klik pada "List Harga" untuk melihat list.
 
 Pembayaran:
-Pulsa : 085730903853
-GoPay: 085730903853
+Pulsa : 085323176492
+GoPay: 085323176492
 `.trim()
     const button = {
         buttonText: 'List Harga',
